b9888089b913c3a0e2cbed65d6b8c5ba5c5c1cb5
newstoryworld421-glitch <newstoryworld421@gmail.com>
2026-06-19 13:29:07 +0530

    Change bot token in bot.py
    Updated the bot token for the application.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("8997747937:AAGpQKTtb2tEoBONXr6eW73TFPPqV_RwnSU")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("⚔️ Hunter X Bot Online!")
 
-app = Application.builder().token(TOKEN).build()
+app = Application.builder().token(8997747937:AAGpQKTtb2tEoBONXr6eW73TFPPqV_RwnSU).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
